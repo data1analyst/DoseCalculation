1e82b491ad5238da62fe91b42a0ddcace5675c4e
Yasmin Hamdi <100616887+data1analyst@users.noreply.github.com>
2023-07-21 22:11:15 +0300

    Update Herceptin.py

--- Herceptin.py
+++ Herceptin.py
@@ -1,24 +1,27 @@
 #import the required module
 from datetime import datetime
 
+#take input from the user
 her_dose=int(input("enter hercepten dose: "))
 her_pre=int(input("how much do we have? "))
 #now we calculate the dose:
 
-calc= her_dose - her_pre
-if her_dose < 440:
-    ml = round(calc/22,2)
-    remain = 440 -calc
-    print("use all of the old one and {} ml from the new one".format(ml))
-    print("write the remain of herceptin {}mg".format(remain))
+if her_dose < her_pre :
     
-elif her_dose > 440:
-    ad= her_dose - 440 
-    if ad > her_pre:
-        re= ad-her_pre
-        print("use the old and new vials and you stil need {}mg equal to {} ml".format(re, round(re/22,2)))
+    ml = round(her_dose/22,2)
+    remain = her_pre - her_dose
+    print("use {} ml from the old one ".format(ml))
+    print("write the remain of herceptin {} mg".format(remain))
+    
+elif her_dose > her_pre:
+    ad = her_dose - her_pre
+    if ad = 0 :
+        print("use all of the old and new one")
     else:
-        print("use the whole new one and take {} ml form the old one".format(ad))
+        calc = round(calc/22)
+        remain = 440 - ad
+        print("use all of the old one and {} ml from the new one".format(calc))
+        print("don't forget to write the remainning herceptin {} mg ".format(remain))
 now = datetime.now()
 current=now.strftime("%I:%M")
 day=datetime.today()
