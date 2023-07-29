2ef9c12d266abe7298574577688cb47c411db135
Yasmin Hamdi <100616887+data1analyst@users.noreply.github.com>
2023-07-29 23:43:36 +0300

    Update Herceptin.py

--- Herceptin.py
+++ Herceptin.py
@@ -4,6 +4,7 @@ from datetime import datetime
 #take input from the user
 her_dose=int(input("enter hercepten dose: "))
 her_pre=int(input("how much do we have? "))
+
 #now we calculate the dose:
 
 if her_dose < her_pre :
@@ -23,14 +24,16 @@ else:
     elif ad == 440:
         print("use all of the old and new one")
     else:
-        calc = round(ad/22)
+        calc = round(ad/22,2)
         remain = 440 - ad
         print("use all of the old one and {} ml from the new one".format(calc))
         print("don't forget to write the remainning herceptin {} mg ".format(remain))
+
 now = datetime.now()
 current=now.strftime("%I:%M")
 day=datetime.today()
 current_day=day.strftime("%d/%m/%Y")
+
 print("don't forget to write the day and date")
 print(current , current_day)
 
