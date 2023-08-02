ae693cbcc3a0551abae874b1a88f7d0bdb75cc59
Yasmin Hamdi <100616887+data1analyst@users.noreply.github.com>
2023-08-03 00:17:22 +0300

    Update Pump.py

--- Pump.py
+++ Pump.py
@@ -3,8 +3,11 @@ while True:
     fl_conc=int(input("What's the conc. of 5-Flu ? "))
     hrs=int(input("For how much hours to pump? "))
     dl=(hrs*2)-(fl_conc/50)
+    fl_ml = fl_conc/50
+    
+    
     if dl > 0:
-        print("you should add {} ml of the diluent".format(dl))
+        print("you should add {} ml of the diluent and {} ml of 5-flu".format(round(dl,2),fl_ml))
     else:
         print("pump will last more than {} without dilution".format(hrs))
     ask=input("do you want to try again? ")
